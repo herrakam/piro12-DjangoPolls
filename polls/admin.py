fe370ec56b714ec9a943843996ff7f7097ada3c5
정승현 <fm1003@naver.com>
2020-01-23 02:00:09 +0900

    7

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
